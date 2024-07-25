e793689e9a7f123748509e37d4794230a333acd8
Sanger Steel <sangersteel@gmail.com>
2024-07-25 16:25:57 -0400

    feat: Hypothesize writing and loading with `.write_state_dict`
    Posits a possible workflow for creating the necessary
    `Dict[str, torch.Tensor]` object needed to be consumed by
    `TensorSerializer.write_state_dict()` and a way to load them in to
    an `ExLlamaV2` model at runtime.

--- get_state_dict.py
+++ get_state_dict.py
@@ -4,9 +4,20 @@ import torch
 import exllamav2.linear
 import exllamav2.embedding
 import exllamav2.rmsnorm
+import exllamav2.model
+
+is_norm = lambda x: x.endswith("norm")
+is_linear = lambda x: x.endswith("proj") or x.endswith("lm_head")
+is_embedding = lambda x: x.endswith("embed_tokens")
+
+get_embedding = lambda x: x.embedding.weight.data
+get_linear = lambda x: x.linear.weight.data
+get_rms_norm = lambda x: x.weight.data
 
 
-
+# Assumes the three types listed here are the *only* ones that need specific
+# handling. This is just a guess made empirically through debugging and assuming
+# Llama architectural sameness
 def get_state_dict(modules_dict: Dict[str, Any]) -> Dict[str, torch.tensor]:
     state_dict = {}
     for key, value in modules_dict:
@@ -19,6 +30,14 @@ def get_state_dict(modules_dict: Dict[str, Any]) -> Dict[str, torch.tensor]:
 
     return state_dict
 
-def load_state_dict_into_model(state_dict: Dict[str, torch.tensor]) -> None:
+# Assumption is keys of modules_dict match one-to-one with state_dict, which should be the case due to how I've
+# defined get_state_dict. Also assumes that `modules_dict` contains the weights that are actually used during
+# forward passes.
+def load_state_dict_into_model(model: exllamav2.model.ExLlamaV2, state_dict: Dict[str, torch.tensor]) -> None:
     for key, value in state_dict:
-        ## TODO: Need some way of knowing how to map a str key like 'model.embed_tokens' to ExLlamaV2Embedding etc
+        if is_embedding(key):
+            model.modules_dict[key].embedding.weight.data = state_dict[key]
+        if is_norm(key):
+            model.modules_dict[key].weight.data = state_dict[key]
+        if is_linear(key):
+            model.modules_dict[key].linear.data = state_dict[key]
