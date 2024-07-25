1ac112570468ee7c03e12a0ae857699b9c3dfc69
Sanger Steel <sangersteel@gmail.com>
2024-07-25 16:51:45 -0400

    docs: Notes on next steps

--- get_state_dict.py
+++ get_state_dict.py
@@ -35,6 +35,9 @@ def get_state_dict(modules_dict: Dict[str, Any]) -> Dict[str, torch.tensor]:
 # forward passes.
 
 # It seems like `model.modules` may be what's used to load the weights. Refer to line 535 of model.py
+# Line 300 might be the key to understanding how they relate. If I have to, I could probably "fill"
+# the weights in `model.modules` with those from `model.modules_dict` using the relationship in line 300
+# if I had to.
 def load_state_dict_into_model(model: exllamav2.model.ExLlamaV2, state_dict: Dict[str, torch.tensor]) -> None:
     for key, value in state_dict:
         if is_embedding(key):
